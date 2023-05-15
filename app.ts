391782ced6aee0d3c4950e7bd54ad04fc7a92494
Martin Doušek <dousek.martin@gmail.com>
2023-05-15 20:15:48 +0200

    19. Working with Tuples

--- app.ts
+++ app.ts
@@ -1,6 +1,22 @@
-const person = {
+const person: {
+    name: string
+    age: 30,
+    hobbies: string[],
+    role: [number, string]
+} = {
     name: "Maxmilian",
-    age: 30
+    age: 30,
+    hobbies: ['sports', 'Cooking'],
+    role: [2, 'author']
 }
 
+person.role.push()
+
+let favouriteAcrivities: string[]
+
 console.log(person)
+
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase())
+}
