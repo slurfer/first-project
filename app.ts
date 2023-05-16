1cd74c57dd0f46b495cfec935afc74aa839209bd
Martin Doušek <dousek.martin@gmail.com>
2023-05-16 17:14:05 +0200

    28. Function Types & Callbacks

--- app.ts
+++ app.ts
@@ -6,6 +6,13 @@ function printResult(num: number): void {
   console.log("Result: " + num);
 }
 
+
+function addAddHandler(n1: number, n2: number, cb: (num: number) => void) {
+    const result = n1 + n2;
+    cb(result)
+}
+
+
 printResult(add(5, 12));
 
 
@@ -14,3 +21,6 @@ let combineValues: (a: number, b: number) => number;
 combineValues = add;
 
 console.log(combineValues(8, 8))
+
+
+addAddHandler(10, 20, (result) => console.log(result))
